12aee7e1babdf4794377d2303ef7411e3acdb94c
LiranBashari <liranbashari1@gmail.com>
2023-04-19 20:47:57 +0300

    server login+register is done

--- server/server.js
+++ server/server.js
@@ -2,10 +2,13 @@ const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose")
 require("dotenv").config();
+const userRoutes = require("./routes/userRoutes")
+
 
 const app = express();
 app.use(express.json());
 app.use(cors());
+app.use("/api", userRoutes)
 
 mongoose.connect(process.env.MONGO_URL, {
     useNewUrlParser: true,
