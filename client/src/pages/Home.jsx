56b552bf6d8cab46c9e5edcdb3f212ee5916bb86
LiranBashari <liranbashari1@gmail.com>
2023-04-21 00:27:47 +0300

    set the css

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,6 +1,7 @@
 import React, {useEffect, useState} from 'react';
 import styled from "styled-components";
 import {useNavigate} from "react-router-dom";
+import Logo from "../logo.svg"
 
 function Home() {
     const [userData, setUserData] = useState({})
@@ -18,9 +19,13 @@ function Home() {
     return (
         <Container >
             <div className="header">
-                <h1>
-                    Welcome {userData.username}!
-                </h1>
+                <div className="details">
+                    <img src={Logo}/>
+                    <h1>{userData.username}</h1>
+                </div>
+                <div className="search-container">
+                    <input className="search" type="text" placeholder="Search for courses..."/>
+                </div>
             </div>
             <div className="courses">
 
@@ -31,17 +36,41 @@ function Home() {
 
 export default Home;
 
+
 const Container = styled.div`
   height: 100vh;
   width: 100vw;
-  background-color:#b7edfc;
-  
-  .header{
-    
+  background-color: #b7edfc;
 
+  .header {
+    background-color: rgba(255, 255, 255, 0.6);
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+
+
+    .details {
+      font-size: small;
+      display: flex;
+      flex-direction: row;
+      color: #299fff;
+
+      img {
+        height: 2rem;
+        padding-top: 1.3rem;
+        padding-right: 0.3rem;
+      }
+    }
+
+    .search-container {
+      margin: 0 auto; /* center the container horizontally */
+      .search {
+        border-radius: 2rem;
+        padding: 0.8rem 2rem 0.8rem 3rem;
+        font-size: larger;
+      }
+    }
   }
-  
-  .courses{
-    
-  }
-    `;
+
+
+`;
